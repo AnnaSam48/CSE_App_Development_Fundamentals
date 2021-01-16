51cbb8c32b5539baccb529cd8b27b6cfff0d819a
Anna Samborska <littlesamurai4@yahoo.co.uk>
2021-01-16 19:09:30 +0200

    Initial commit

---
+++ src/main/java/com/accenture/applicationDevelopmentFundamentals/MovieRatingApp/MovieRatingAppApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.applicationDevelopmentFundamentals.MovieRatingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingAppApplication.class, args);
+	}
+
+}
